b010688d8f4bb88d45dcc2417d13b87725fb91f5
Alexander Shevelev <al.e.shevelev@gmail.com>
2024-05-31 21:56:50 +0300

    The algorithm for getting all the cells around some other cell in radius N has bee added to the project

--- trench_warfare/lib/core_entities/entities/hex_matrix/hex_matrix.dart
+++ trench_warfare/lib/core_entities/entities/hex_matrix/hex_matrix.dart
@@ -26,13 +26,10 @@ abstract class HexMatrix<T extends HexMatrixItem> {
   Iterable<T?> findAllCellsAround(T centralCell) {
     final result = List<T?>.empty(growable: true);
 
-    final rowRange = Range(0, rows - 1);
-    final colRange = Range(0, cols - 1);
-
     // top-right
     final row4 = centralCell.row - 1;
     final col4 = centralCell.row % 2 == 0 ? centralCell.col : centralCell.col + 1;
-    if (rowRange.isInRange(row4, minIncluded: true) && colRange.isInRange(col4, minIncluded: true) ) {
+    if (_isInsideMatrix(row: row4, col: col4)) {
       result.add(getCell(row4, col4));
     } else {
       result.add(null);
@@ -41,7 +38,7 @@ abstract class HexMatrix<T extends HexMatrixItem> {
     // right
     final row1 = centralCell.row;
     final col1 = centralCell.col + 1;
-    if (rowRange.isInRange(row1, minIncluded: true) && colRange.isInRange(col1, minIncluded: true) ) {
+    if (_isInsideMatrix(row: row1, col: col1)) {
       result.add(getCell(row1, col1));
     } else {
       result.add(null);
@@ -50,7 +47,7 @@ abstract class HexMatrix<T extends HexMatrixItem> {
     // bottom-right
     final row6 = centralCell.row + 1;
     final col6 = centralCell.row % 2 == 0 ? centralCell.col : centralCell.col + 1;
-    if (rowRange.isInRange(row6, minIncluded: true) && colRange.isInRange(col6, minIncluded: true) ) {
+    if (_isInsideMatrix(row: row6, col: col6)) {
       result.add(getCell(row6, col6));
     } else {
       result.add(null);
@@ -59,7 +56,7 @@ abstract class HexMatrix<T extends HexMatrixItem> {
     // bottom-left
     final row5 = centralCell.row + 1;
     final col5 = centralCell.row % 2 == 0 ? centralCell.col - 1 : centralCell.col;
-    if (rowRange.isInRange(row5, minIncluded: true) && colRange.isInRange(col5, minIncluded: true) ) {
+    if (_isInsideMatrix(row: row5, col: col5)) {
       result.add(getCell(row5, col5));
     } else {
       result.add(null);
@@ -68,7 +65,7 @@ abstract class HexMatrix<T extends HexMatrixItem> {
     // left
     final row2 = centralCell.row;
     final col2 = centralCell.col - 1;
-    if (rowRange.isInRange(row2, minIncluded: true) && colRange.isInRange(col2, minIncluded: true) ) {
+    if (_isInsideMatrix(row: row2, col: col2)) {
       result.add(getCell(row2, col2));
     } else {
       result.add(null);
@@ -77,7 +74,7 @@ abstract class HexMatrix<T extends HexMatrixItem> {
     // top-left
     final row3 = centralCell.row - 1;
     final col3 = centralCell.row % 2 == 0 ? centralCell.col - 1 : centralCell.col;
-    if (rowRange.isInRange(row3, minIncluded: true) && colRange.isInRange(col3, minIncluded: true) ) {
+    if (_isInsideMatrix(row: row3, col: col3)) {
       result.add(getCell(row3, col3));
     } else {
       result.add(null);
@@ -86,11 +83,122 @@ abstract class HexMatrix<T extends HexMatrixItem> {
     return result;
   }
 
-  /// Returns all cells except out of the field
+  /// Returns all cells except the cells out of the field
   Iterable<T> findCellsAround(T centralCell) =>
       findAllCellsAround(centralCell).where((e) => e != null).map((e) => e!).toList();
+
+  /// Returns all cells (except the cells out of the field)
+  /// around started from top-right in the radius, clockwise
+  /// [radius] is in interval [1, N]
+  Iterable<T> findCellsAroundR(T centralCell, {required int radius}) {
+    final result = List<T>.empty(growable: true);
+
+    if (radius <= 0) {
+      return result;
+    }
+
+    // the top-right cell
+    var baseRow = centralCell.row - radius;
+    var baseCol = centralCell.col + (centralCell.row % 2 == 1 ? (radius / 2).ceil() : (radius / 2).floor());
+    if (_isInsideMatrix(row: baseRow, col: baseCol)) {
+      result.add(getCell(baseRow, baseCol));
+    }
+
+    // the top-right side
+    for (var i = 1; i <= radius; i++) {
+      var row = baseRow + i;
+      var col = baseCol + (baseRow % 2 == 1 ? (i / 2).ceil() : (i / 2).floor());
+      if (_isInsideMatrix(row: row, col: col)) {
+        result.add(getCell(row, col));
+      }
+
+      // the right cell
+      if (i == radius) {
+        baseRow = row;
+        baseCol = col;
+      }
+    }
+
+    // the bottom-right side
+    for (var i = 1; i <= radius; i++) {
+      var row = baseRow + i;
+      var col = baseCol - (baseRow % 2 == 1 ? (i / 2).floor() : (i / 2).ceil());
+      if (_isInsideMatrix(row: row, col: col)) {
+        result.add(getCell(row, col));
+      }
+
+      // the bottom-right cell
+      if (i == radius) {
+        baseRow = row;
+        baseCol = col;
+      }
+    }
+
+    // the bottom side
+    for (var i = 1; i <= radius; i++) {
+      var row = baseRow;
+      var col = baseCol - i;
+      if (_isInsideMatrix(row: row, col: col)) {
+        result.add(getCell(row, col));
+      }
+
+      // the bottom-left cell
+      if (i == radius) {
+        baseRow = row;
+        baseCol = col;
+      }
+    }
+
+    // the bottom-left side
+    for (var i = 1; i <= radius; i++) {
+      var row = baseRow - i;
+      var col = baseCol - (baseRow % 2 == 1 ? (i / 2).floor() : (i / 2).ceil());
+      if (_isInsideMatrix(row: row, col: col)) {
+        result.add(getCell(row, col));
+      }
+
+      // the left cell
+      if (i == radius) {
+        baseRow = row;
+        baseCol = col;
+      }
+    }
+
+    // the top-left side
+    for (var i = 1; i <= radius; i++) {
+      var row = baseRow - i;
+      var col = baseCol + (baseRow % 2 == 1 ? (i / 2).ceil() : (i / 2).floor());
+      if (_isInsideMatrix(row: row, col: col)) {
+        result.add(getCell(row, col));
+      }
+
+      // the top-left cell
+      if (i == radius) {
+        baseRow = row;
+        baseCol = col;
+      }
+    }
+
+    // the top side
+    for (var i = 1; i < radius; i++) {
+      var row = baseRow;
+      var col = baseCol + i;
+      if (_isInsideMatrix(row: row, col: col)) {
+        result.add(getCell(row, col));
+      }
+    }
+
+    return result;
+  }
 
   /// Calculates a logical (in terms of rows and cols) distance between the cells
   double calculateDistance(T cell1, T cell2) =>
       math.sqrt(math.pow(cell1.row - cell2.row, 2) + math.pow(cell1.col - cell2.col, 2));
+
+  bool _isInsideMatrix({required int row, required int col}) {
+    final rowRange = Range(0, rows - 1);
+    final colRange = Range(0, cols - 1);
+
+    return rowRange.isInRange(row, minIncluded: true) && colRange.isInRange(col, minIncluded: true);
+  }
 }
---
+++ trench_warfare/test/hex_matrix_test.dart
@@ -0,0 +1,294 @@
+import 'package:test/test.dart';
+import 'package:trench_warfare/core_entities/entities/hex_matrix/hex_matrix.dart';
+import 'package:trench_warfare/core_entities/entities/hex_matrix/hex_matrix_item.dart';
+import 'package:trench_warfare/shared/helpers/extensions.dart';
+
+import 'assert.dart';
+
+class TestHexMatrixItem extends HexMatrixItem {
+  TestHexMatrixItem({required super.row, required super.col});
+}
+
+class TestHexMatrix extends HexMatrix<TestHexMatrixItem> {
+  TestHexMatrix(super.cellsRaw, {required super.rows, required super.cols});
+}
+
+TestHexMatrix _createHexMatrix() {
+  const rows = 7;
+  const cols = 7;
+
+  final List<TestHexMatrixItem> cells = [];
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      cells.add(TestHexMatrixItem(row: i, col: j));
+    }
+  }
+
+  return TestHexMatrix(cells, rows: rows, cols: cols);
+}
+
+void main() {
+  group('HexMatrix normal cases', () {
+    test('start row is odd', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(3, 3);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 12);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 1 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 2 && e.col == 5)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 3 && e.col == 5)!);
+
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 4 && e.col == 5)!);
+      Assert.isTrue(result.elementAt(4).let((e) => e.row == 5 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(5).let((e) => e.row == 5 && e.col == 3)!);
+
+      Assert.isTrue(result.elementAt(6).let((e) => e.row == 5 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(7).let((e) => e.row == 4 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(8).let((e) => e.row == 3 && e.col == 1)!);
+
+      Assert.isTrue(result.elementAt(9).let((e) => e.row == 2 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(10).let((e) => e.row == 1 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(11).let((e) => e.row == 1 && e.col == 3)!);
+    });
+
+    test('start row is even', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(2, 3);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 12);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 0 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 1 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 2 && e.col == 5)!);
+
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 3 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(4).let((e) => e.row == 4 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(5).let((e) => e.row == 4 && e.col == 3)!);
+
+      Assert.isTrue(result.elementAt(6).let((e) => e.row == 4 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(7).let((e) => e.row == 3 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(8).let((e) => e.row == 2 && e.col == 1)!);
+
+      Assert.isTrue(result.elementAt(9).let((e) => e.row == 1 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(10).let((e) => e.row == 0 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(11).let((e) => e.row == 0 && e.col == 3)!);
+    });
+  });
+
+  group('HexMatrix radius cases', () {
+    test('the radius is zero', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(3, 3);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 0);
+
+      // Assert
+      Assert.isTrue(result.isEmpty);
+    });
+
+    test('the radius is 1', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(3, 3);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 1);
+
+      // Assert
+      Assert.isTrue(result.length == 6);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 2 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 3 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 4 && e.col == 4)!);
+
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 4 && e.col == 3)!);
+      Assert.isTrue(result.elementAt(4).let((e) => e.row == 3 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(5).let((e) => e.row == 2 && e.col == 3)!);
+    });
+
+    test('the radius is extra large', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(3, 3);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 10);
+
+      // Assert
+      Assert.isTrue(result.isEmpty);
+    });
+  });
+
+  group('HexMatrix corner cases', () {
+    test('left-top corner', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(0, 0);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 4);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 0 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 1 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 2 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 2 && e.col == 0)!);
+    });
+
+    test('right-top corner', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(0, 6);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 4);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 2 && e.col == 6)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 2 && e.col == 5)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 1 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 0 && e.col == 4)!);
+    });
+
+    test('right-bottom corner', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(6, 6);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 4);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 6 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 5 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 4 && e.col == 5)!);
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 4 && e.col == 6)!);
+    });
+
+    test('left-bottom corner', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(6, 0);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 4);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 4 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 5 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 6 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 4 && e.col == 0)!);
+    });
+  });
+
+  group('HexMatrix side cases', () {
+    test('top side', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(0, 3);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 7);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 0 && e.col == 5)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 1 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 2 && e.col == 4)!);
+
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 2 && e.col == 3)!);
+      Assert.isTrue(result.elementAt(4).let((e) => e.row == 2 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(5).let((e) => e.row == 1 && e.col == 1)!);
+
+      Assert.isTrue(result.elementAt(6).let((e) => e.row == 0 && e.col == 1)!);
+    });
+
+    test('right side', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(3, 6);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 7);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 5 && e.col == 6)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 5 && e.col == 5)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 4 && e.col == 5)!);
+
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 3 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(4).let((e) => e.row == 2 && e.col == 5)!);
+      Assert.isTrue(result.elementAt(5).let((e) => e.row == 1 && e.col == 5)!);
+
+      Assert.isTrue(result.elementAt(6).let((e) => e.row == 1 && e.col == 6)!);
+    });
+
+    test('bottom side', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(6, 3);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 7);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 4 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 5 && e.col == 4)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 6 && e.col == 5)!);
+
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 6 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(4).let((e) => e.row == 5 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(5).let((e) => e.row == 4 && e.col == 2)!);
+
+      Assert.isTrue(result.elementAt(6).let((e) => e.row == 4 && e.col == 3)!);
+    });
+
+    test('left side', () {
+      // Arrange
+      final matrix = _createHexMatrix();
+      final centralCell = matrix.getCell(3, 0);
+
+      // Act
+      final result = matrix.findCellsAroundR(centralCell, radius: 2);
+
+      // Assert
+      Assert.isTrue(result.length == 7);
+
+      Assert.isTrue(result.elementAt(0).let((e) => e.row == 1 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(1).let((e) => e.row == 2 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(2).let((e) => e.row == 3 && e.col == 2)!);
+
+      Assert.isTrue(result.elementAt(3).let((e) => e.row == 4 && e.col == 2)!);
+      Assert.isTrue(result.elementAt(4).let((e) => e.row == 5 && e.col == 1)!);
+      Assert.isTrue(result.elementAt(5).let((e) => e.row == 5 && e.col == 0)!);
+
+      Assert.isTrue(result.elementAt(6).let((e) => e.row == 1 && e.col == 0)!);
+    });
+  });
+}
