7cc4f4a3c8d562e5b741bd34debaea3f2dd3d442
Alexander Shevelev <al.e.shevelev@gmail.com>
2024-06-24 23:12:07 +0300

    The estimator for production center has been added to the aggressive AI

--- trench_warfare/lib/screens/game_field_screen/model/domain/player/ai/aggressive/aggressive_player_ai_library.dart
+++ trench_warfare/lib/screens/game_field_screen/model/domain/player/ai/aggressive/aggressive_player_ai_library.dart
@@ -3,11 +3,17 @@ library aggressive_player_ai;
 import 'dart:math' as math;
 
 import 'package:flutter/foundation.dart';
+import 'package:trench_warfare/core_entities/entities/game_objects/game_object.dart';
+import 'package:trench_warfare/core_entities/entities/money/money_unit.dart';
 import 'package:trench_warfare/core_entities/enums/nation.dart';
+import 'package:trench_warfare/core_entities/enums/production_center_type.dart';
 import 'package:trench_warfare/screens/game_field_screen/model/data/readers/metadata/dto/map_metadata.dart';
+import 'package:trench_warfare/screens/game_field_screen/model/domain/build/build_calculators_library.dart';
 import 'package:trench_warfare/screens/game_field_screen/model/domain/game_field/game_field_library.dart';
 import 'package:trench_warfare/screens/game_field_screen/model/domain/money/money_storage.dart';
 import 'package:trench_warfare/screens/game_field_screen/model/domain/player/ai/player_ai.dart';
+import 'package:trench_warfare/screens/game_field_screen/model/domain/player/estimations/estimations.dart';
+import 'package:trench_warfare/screens/game_field_screen/model/domain/player/influence_map/influence_map_library.dart';
 import 'package:trench_warfare/screens/game_field_screen/model/domain/player/player_library.dart';
 
 part 'aggressive_player_ai.dart';
--- trench_warfare/lib/screens/game_field_screen/model/domain/player/ai/aggressive/phases/money_spending/estimations/production_center_estimation.dart
+++ trench_warfare/lib/screens/game_field_screen/model/domain/player/ai/aggressive/phases/money_spending/estimations/production_center_estimation.dart
@@ -1,19 +1,15 @@
 part of aggressive_player_ai;
 
-/*
-class ProductionCenterEstimationResult extends EstimationResult {
-  final Iterable<GameFieldCellRead> cellsPossibleToBuild;
+class ProductionCenterEstimationData {
+  final GameFieldCellRead cell;
 
-  ProductionCenterEstimationResult(
-      super.weight, {
-        required this.cellsPossibleToBuild,
-      });
+  final ProductionCenterType type;
+
+  ProductionCenterEstimationData({required this.cell, required this.type});
 }
-*/
 
 /// Should we build production center on not in general?
-/*
-class ProductionCenterEstimator implements Estimator<ProductionCenterEstimationResult> {
+class ProductionCenterEstimator implements Estimator<ProductionCenterEstimationData> {
   final GameFieldRead _gameField;
 
   final Nation _myNation;
@@ -28,19 +24,19 @@ class ProductionCenterEstimator implements Estimator<ProductionCenterEstimationR
 
   bool get _isLand => _type != ProductionCenterType.navalBase;
 
-  double get _correctionFactor => switch(_type) {
-    ProductionCenterType.navalBase =>  4.0,
-    ProductionCenterType.city =>  2.0,
-    ProductionCenterType.factory =>  2.0,
-    ProductionCenterType.airField =>  3.0,
-  };
+  double get _correctionFactor => switch (_type) {
+        ProductionCenterType.navalBase => 4.0,
+        ProductionCenterType.city => 2.0,
+        ProductionCenterType.factory => 2.0,
+        ProductionCenterType.airField => 3.0,
+      };
 
-  double get _maxFractionCellWithPCs => switch(_type) {
-    ProductionCenterType.navalBase =>  0.05,
-    ProductionCenterType.city =>  0.1,
-    ProductionCenterType.factory =>  0.1,
-    ProductionCenterType.airField =>  0.075,
-  };
+  double get _maxFractionCellWithPCs => switch (_type) {
+        ProductionCenterType.navalBase => 0.05,
+        ProductionCenterType.city => 0.1,
+        ProductionCenterType.factory => 0.1,
+        ProductionCenterType.airField => 0.075,
+      };
 
   ProductionCenterEstimator({
     required GameFieldRead gameField,
@@ -57,13 +53,13 @@ class ProductionCenterEstimator implements Estimator<ProductionCenterEstimationR
         _metadata = metadata;
 
   @override
-  ProductionCenterEstimationResult estimate() {
+  Iterable<EstimationResult<ProductionCenterEstimationData>> estimate() {
     final buildCalculator = ProductionCentersBuildCalculator(_gameField, _myNation);
     final allCellsPossibleToBuild = buildCalculator.getAllCellsPossibleToBuild(_type, _nationMoney);
 
     // We can't build shit
     if (allCellsPossibleToBuild.isEmpty) {
-      return ProductionCenterEstimationResult(0, cellsPossibleToBuild: []);
+      return [];
     }
 
     final allAggressors = _metadata.getAllAggressive().where((a) => a != _myNation).toList(growable: true);
@@ -71,7 +67,7 @@ class ProductionCenterEstimator implements Estimator<ProductionCenterEstimationR
       final cellFromMap = _influenceMap.getItem(c.row, c.col);
 
       for (final aggressor in allAggressors) {
-        if (cellFromMap.getCombined(aggressor) >  cellFromMap.getCombined(_myNation)) {
+        if (cellFromMap.getCombined(aggressor) > cellFromMap.getCombined(_myNation)) {
           return false;
         }
       }
@@ -81,7 +77,7 @@ class ProductionCenterEstimator implements Estimator<ProductionCenterEstimationR
 
     // It's a dangerous time, we shouldn't build production centers in a moment
     if (allSafeCells.isEmpty) {
-      return ProductionCenterEstimationResult(0, cellsPossibleToBuild: []);
+      return [];
     }
 
     var allOurCellsCount = 0;
@@ -105,13 +101,13 @@ class ProductionCenterEstimator implements Estimator<ProductionCenterEstimationR
     if (allOurCellsWithPC.length.toDouble() / allOurCellsCount > _maxFractionCellWithPCs) {
       final pcWithoutMaxLevel = allOurCellsWithPC
           .where((c) =>
-      c.productionCenter!.level != ProductionCenter.getMaxLevel(c.productionCenter!.type) &&
-          allSafeCells.contains(c))
+              c.productionCenter!.level != ProductionCenter.getMaxLevel(c.productionCenter!.type) &&
+              allSafeCells.contains(c))
           .toList(growable: false);
 
       // All production centers are upgraded - that's all
       if (pcWithoutMaxLevel.isEmpty) {
-        return ProductionCenterEstimationResult(0, cellsPossibleToBuild: []);
+        return [];
       } else {
         allSafeCells = pcWithoutMaxLevel;
       }
@@ -120,7 +116,14 @@ class ProductionCenterEstimator implements Estimator<ProductionCenterEstimationR
     final resultWeight = allOurCellsWithPC.isEmpty
         ? 10.0
         : (math.sqrt(allOurCellsCount.toDouble() / allOurCellsWithPC.length) - 1) / _correctionFactor;
-    return ProductionCenterEstimationResult(resultWeight, cellsPossibleToBuild: allSafeCells);
+    return allSafeCells
+        .map((c) => EstimationResult<ProductionCenterEstimationData>(
+              weight: resultWeight,
+              data: ProductionCenterEstimationData(
+                cell: c,
+                type: _type,
+              ),
+            ))
+        .toList(growable: false);
   }
 }
-*/
