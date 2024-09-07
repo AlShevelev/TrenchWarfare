8d827e9eff568a1b516fe5b189985541b6be2e4b
Alexander Shevelev <al.e.shevelev@gmail.com>
2024-09-07 21:22:35 +0300

    A new test map has been added to the project

--- trench_warfare/lib/screens/start_screen/start_screen.dart
+++ trench_warfare/lib/screens/start_screen/start_screen.dart
@@ -14,7 +14,8 @@ class StartScreen extends StatelessWidget {
             onPressed: () {
               //Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/15x15_carriers_enemy_pc_path_is_too_long.tmx');
               //Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/15x15_carriers_enemy_pc_reachable_by_water_only.tmx');
-              Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/40x40_land.tmx');
+              Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/15x15_carriers_enemy_pc_path_is_too_dungerous.tmx');
+              //Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/40x40_land.tmx');
               //Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/7x7_win_defeat_conditions.tmx');
             },
             child: const Text(
