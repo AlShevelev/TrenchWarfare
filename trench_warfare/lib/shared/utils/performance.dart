cd5a995c0528e26a91db65326d4431039905156d
Alexander Shevelev <al.e.shevelev@gmail.com>
2024-09-10 22:57:53 +0300

    A new performance counter has been added to the project

--- trench_warfare/lib/shared/utils/performance.dart
+++ trench_warfare/lib/shared/utils/performance.dart
@@ -1,9 +1,15 @@
-import 'dart:developer' as developer;
-
 T estimateCallTime<T>(T Function() action, {String tag = ""}) {
   final stopwatch = Stopwatch()..start();
   final result = action();
-  developer.log('${tag}The function executed in ${stopwatch.elapsed}');
+  print('$tag: The function executed in ${stopwatch.elapsed}');
+
+  return result;
+}
+
+Future<T> estimateCallTimeAsync<T>(Future<T> Function() action, {String tag = ""}) async {
+  final stopwatch = Stopwatch()..start();
+  final result = await action();
+  print('$tag: The function executed in ${stopwatch.elapsed}');
 
   return result;
 }
