d8cfeaa4f4e05c1f5aef026d1d8d8b4708c84298
Alexander Shevelev <al.e.shevelev@gmail.com>
2024-10-02 22:17:16 +0300

    The test map has been updated

--- trench_warfare/lib/screens/start_screen/start_screen.dart
+++ trench_warfare/lib/screens/start_screen/start_screen.dart
@@ -17,7 +17,8 @@ class StartScreen extends StatelessWidget {
                 onPressed: () {
                   //Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/15x15_carriers_enemy_pc_reachable_by_water_only.tmx');
                   //Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/40x40_land.tmx');
-                  Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/7x7_expenses_test_1.tmx');
+                  //Navigator.of(context).pushNamed(Routes.gameField, arguments: 'test/7x7_expenses_test_1.tmx');
+                  Navigator.of(context).pushNamed(Routes.gameField, arguments: 'real/europe/the_battle_of_tannenburg.tmx');
                 },
                 child: const Text(
                   "Start",
