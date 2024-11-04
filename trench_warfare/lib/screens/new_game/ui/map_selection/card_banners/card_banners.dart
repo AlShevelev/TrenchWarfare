75545c5bc3f8a3b4431c90eed209c650134e00af
Alexander Shevelev <al.e.shevelev@gmail.com>
2024-11-04 23:08:19 +0300

    A static for the new game widgets has been completed

---
+++ trench_warfare/lib/screens/new_game/ui/map_selection/card_banners/card_banners.dart
@@ -0,0 +1,53 @@
+part of map_selection_ui;
+
+class CardBanners extends StatelessWidget {
+  final MapCardDto _card;
+
+  static const _bannerSize = 50.0;
+  static const _opponentSelectionWidth = 3.0;
+
+  const CardBanners({super.key, required MapCardDto card}) : _card = card;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_card.neutrals.isEmpty) {
+      return _getOpponents();
+    } else {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _getOpponents(),
+          _getBannersColumn(
+            header: tr('new_game_neutrals'),
+            body: CardBannersNeutrals(
+              neutrals: _card.neutrals,
+              bannerSize: _bannerSize,
+            ),
+          ),
+        ],
+      );
+    }
+  }
+
+  Widget _getBannersColumn({required String header, required Widget body}) => Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+            child: Text(
+              header,
+              style: AppTypography.s16w600,
+            ),
+          ),
+          body,
+        ],
+      );
+
+  Widget _getOpponents() => _getBannersColumn(
+        header: tr('new_game_opponents'),
+        body: CardBannersOpponents(
+          opponents: _card.opponents,
+          bannerSize: _bannerSize,
+          opponentSelectionWidth: _opponentSelectionWidth,
+        ),
+      );
+}
