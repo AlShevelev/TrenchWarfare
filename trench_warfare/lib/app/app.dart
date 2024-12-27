58dc5055047575d07a7c545eef5e3a67d083cd80
Alexander Shevelev <al.e.shevelev@gmail.com>
2024-12-27 20:52:06 +0300

    The audio controller has been started on the app start; music tracks have been edited

--- trench_warfare/lib/app/app.dart
+++ trench_warfare/lib/app/app.dart
@@ -1,13 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:trench_warfare/app/app_lifecycle_observer.dart';
 import 'package:trench_warfare/app/navigation/navigation_library.dart';
 import 'package:trench_warfare/app/theme/theme.dart';
+import 'package:trench_warfare/audio/audio_library.dart';
 import 'package:trench_warfare/screens/cover/cover_screen.dart';
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  const App({super.key});
 
   // This widget is the root of your application.
   @override
@@ -18,14 +20,30 @@ class App extends StatelessWidget {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
     return AppLifecycleObserver(
-      child: MaterialApp(
-        title: 'Trench warfare',
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-        theme: AppThemeFactory.defaultTheme(),
-        onGenerateRoute: (settings) => Routes.createPageRouteBuilder(settings),
-        home: const CoverScreen(),
+      child: MultiProvider(
+        providers: [
+          ProxyProvider<ValueNotifier<AppLifecycleState>, AudioController>(
+            lazy: false,
+            create: (context) => AudioController()..init(),
+            update: (context, lifecycleNotifier, audio) {
+              if (audio == null) {
+                throw ArgumentError.notNull();
+              }
+              audio.attachLifecycleNotifier(lifecycleNotifier);
+              return audio;
+            },
+            dispose: (context, audio) => audio.dispose(),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Trench warfare',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          theme: AppThemeFactory.defaultTheme(),
+          onGenerateRoute: (settings) => Routes.createPageRouteBuilder(settings),
+          home: const CoverScreen(),
+        ),
       ),
     );
   }
