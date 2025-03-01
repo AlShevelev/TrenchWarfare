90d65e51c37b4d28d7e423fe71e34d42890770eb
Alexander Shevelev <al.e.shevelev@gmail.com>
2025-03-01 14:33:33 +0300

    isInDefenceMode - the last streak

--- trench_warfare/lib/screens/cover/cover_screen.dart
+++ trench_warfare/lib/screens/cover/cover_screen.dart
@@ -90,8 +90,8 @@ class _CoverScreenState extends State<CoverScreen> {
                   onPress: () {
                     Navigator.of(context).pushNamed(Routes.fromMapSelectionToGameFieldNewGame,
                         arguments: NewGameToGameFieldNavArg(
-                          mapName: 'assets/tiles/test/7x7_win_defeat_conditions.tmx',
-                          selectedNation: Nation.russia,
+                          mapName: 'assets/tiles/test/10x10_is_defence.tmx',
+                          selectedNation: Nation.austriaHungary,
                         ));
                   },
                 ),
