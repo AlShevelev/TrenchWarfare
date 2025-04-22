58667230686a853210252c8668b8358698d587b0
Alexander Shevelev <al.e.shevelev@gmail.com>
2025-04-22 20:05:40 +0300

    MoneyConstants has been updated

--- trench_warfare/lib/screens/game_field_screen/model/domain/money/calculators/constants/money_constants.dart
+++ trench_warfare/lib/screens/game_field_screen/model/domain/money/calculators/constants/money_constants.dart
@@ -10,35 +10,35 @@ class _MoneyConstants {
   static final cellSnowIncome = MoneyUnit(currency: 1, industryPoints: 0);
   static final cellWaterIncome = MoneyUnit(currency: 2, industryPoints: 0);
 
-  static final unitLandBaseCost = MoneyUnit(currency: 15, industryPoints: 3);
-  static final unitSeaBaseCost = MoneyUnit(currency: 20, industryPoints: 5);
+  static final unitLandBaseCost = MoneyUnit(currency: 15, industryPoints: 6);
+  static final unitSeaBaseCost = MoneyUnit(currency: 20, industryPoints: 10);
   static const unitExpenseFactor = 4.0;
 
-  static final cityBuildCost = MoneyUnit(currency: 1000, industryPoints: 0);
-  static final factoryBuildCost = MoneyUnit(currency: 1000, industryPoints: 0);
-  static final airFieldBuildCost = MoneyUnit(currency: 500, industryPoints: 0);
-  static final navalBaseBuildCost = MoneyUnit(currency: 750, industryPoints: 0);
+  static final cityBuildCost = MoneyUnit(currency: 1000, industryPoints: 100);
+  static final factoryBuildCost = MoneyUnit(currency: 1000, industryPoints: 250);
+  static final airFieldBuildCost = MoneyUnit(currency: 500, industryPoints: 125);
+  static final navalBaseBuildCost = MoneyUnit(currency: 750, industryPoints: 200);
 
-  static final cityIncome = MoneyUnit(currency: 50, industryPoints: 10);
-  static final factoryIncome = MoneyUnit(currency: 10, industryPoints: 50);
+  static final cityIncome = MoneyUnit(currency: 50, industryPoints: 15);
+  static final factoryIncome = MoneyUnit(currency: 10, industryPoints: 75);
   static final airFieldIncome = MoneyUnit(currency: 0, industryPoints: 0);
-  static final navalBaseIncome = MoneyUnit(currency: 30, industryPoints: 30);
+  static final navalBaseIncome = MoneyUnit(currency: 30, industryPoints: 45);
 
-  static final antiAirGunBuildCost = MoneyUnit(currency: 105, industryPoints: 21);
-  static final barbedWireBuildCost = MoneyUnit(currency: 20, industryPoints: 4);
-  static final landFortBuildCost = MoneyUnit(currency: 250, industryPoints: 50);
-  static final landMineBuildCost = MoneyUnit(currency: 36, industryPoints: 7);
-  static final seaMineBuildCost = MoneyUnit(currency: 48, industryPoints: 12);
-  static final trenchBuildCost = MoneyUnit(currency: 20, industryPoints: 4);
+  static final antiAirGunBuildCost = MoneyUnit(currency: 105, industryPoints: 42);
+  static final barbedWireBuildCost = MoneyUnit(currency: 20, industryPoints: 8);
+  static final landFortBuildCost = MoneyUnit(currency: 250, industryPoints: 100);
+  static final landMineBuildCost = MoneyUnit(currency: 36, industryPoints: 14);
+  static final seaMineBuildCost = MoneyUnit(currency: 48, industryPoints: 24);
+  static final trenchBuildCost = MoneyUnit(currency: 20, industryPoints: 8);
 
-  static final attackBoosterBuildCost = MoneyUnit(currency: 15, industryPoints: 3);
-  static final defenceBoosterBuildCost = MoneyUnit(currency: 15, industryPoints: 3);
-  static final commanderBoosterBuildCost = MoneyUnit(currency: 45, industryPoints: 9);
-  static final transportBoosterBuildCost = MoneyUnit(currency: 20, industryPoints: 4);
+  static final attackBoosterBuildCost = MoneyUnit(currency: 15, industryPoints: 6);
+  static final defenceBoosterBuildCost = MoneyUnit(currency: 15, industryPoints: 6);
+  static final commanderBoosterBuildCost = MoneyUnit(currency: 45, industryPoints: 18);
+  static final transportBoosterBuildCost = MoneyUnit(currency: 20, industryPoints: 8);
 
-  static final gasAttackCost = MoneyUnit(currency: 180, industryPoints: 36);
-  static final flechettesCost = MoneyUnit(currency: 90, industryPoints: 20);
-  static final airBombardmentCost = MoneyUnit(currency: 350, industryPoints: 70);
-  static final flameTroopersCost = MoneyUnit(currency: 60, industryPoints: 12);
+  static final gasAttackCost = MoneyUnit(currency: 180, industryPoints: 72);
+  static final flechettesCost = MoneyUnit(currency: 90, industryPoints: 40);
+  static final airBombardmentCost = MoneyUnit(currency: 350, industryPoints: 140);
+  static final flameTroopersCost = MoneyUnit(currency: 60, industryPoints: 24);
   static final propagandaCost = MoneyUnit(currency: 100, industryPoints: 0);
 }
