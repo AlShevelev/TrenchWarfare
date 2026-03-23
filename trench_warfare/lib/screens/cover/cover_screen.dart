18eb98a5560b316ea2ef8fa26719f764db629640
Alexander Shevelev <al.e.shevelev@gmail.com>
2026-03-23 22:23:18 +0300

    Attacking unreachable cell has been remastered

--- trench_warfare/lib/screens/cover/cover_screen.dart
+++ trench_warfare/lib/screens/cover/cover_screen.dart
@@ -101,8 +101,8 @@ class _CoverScreenState extends State<CoverScreen> {
                   onPress: () {
                     Navigator.of(context).pushNamed(Routes.fromMapSelectionToGameFieldNewGame,
                         arguments: NewGameToGameFieldNavArg(
-                          mapName: 'assets/tiles/test/10x10_attack_unreachable_unit_land.tmx',
-                          selectedNation: Nation.austriaHungary,
+                          mapName: 'assets/tiles/test/10x10_attack_unreachable_unit_ai.tmx',
+                          selectedNation: Nation.russia,
                         ));
                   },
                 ),
